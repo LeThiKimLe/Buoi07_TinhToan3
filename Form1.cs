4bdc90e996228ffc74c3375808b02b43f9cbbcf7
KimLe <kimle020102@gmail.com>
2023-09-28 01:05:34 +0700

    Sửa lỗi hiện thông báo khi Thoát mà có lỗi nhập số

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,9 @@ namespace Buoi07_TinhToan3
 {
     public partial class Form1 : Form
     {
+        private bool hasError1 = false;
+        private bool hasError2 = false;
+        private int count = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +28,7 @@ namespace Buoi07_TinhToan3
 
         private void btnThoat_Click(object sender, EventArgs e)
         {
+
             DialogResult dr;
             dr = MessageBox.Show("Bạn có thực sự muốn thoát không?",
                                  "Thông báo", MessageBoxButtons.YesNo);
@@ -66,18 +70,21 @@ namespace Buoi07_TinhToan3
             txtSo1.SelectAll();
         }
 
-
         private void txtSo1_Leave(object sender, EventArgs e)
         {
+           
             string input = txtSo1.Text;
 
             // Kiểm tra xem chuỗi nhập vào có phải là số hay không
             if (!double.TryParse(input, out _))
             {
-                MessageBox.Show("Lỗi: Vui lòng chỉ nhập số!");
-                txtSo1.Focus();
-                txtSo1.SelectAll();
+                hasError1 = true;
+                count = 0;
             }
+            else
+            {
+                hasError1 = false;
+            }    
         }
        
 
@@ -88,11 +95,59 @@ namespace Buoi07_TinhToan3
             // Kiểm tra xem chuỗi nhập vào có phải là số hay không
             if (!double.TryParse(input, out _))
             {
+                hasError2 = true;
+                count = 0;
+            }
+            else {
+                hasError2 = false;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                if (this.Focused == true)
+                {
+                    checkError();
+                }
+
+                foreach(Control ctrl in this.Controls)
+                {
+                    if (ctrl.Focused == true && ctrl.Name != "btnThoat")
+                    {
+                        checkError();
+                    }
+                }
+                
+                foreach(Control ctrl in groupBox1.Controls)
+                {
+                    if (ctrl.Focused == true)
+                    {
+                        checkError();
+                    }
+                }    
+            }
+        }
+
+        private void checkError()
+        {
+            if (hasError1 && txtSo1.Focused == false)
+            {
+                count = count + 1;
+                MessageBox.Show("Lỗi: Vui lòng chỉ nhập số! Số không quá lớn.");
+                txtSo1.Focus();
+                txtSo1.SelectAll();
+            }
+
+            if (hasError2 && txtSo2.Focused == false)
+            {
+                count = count + 1;
                 MessageBox.Show("Lỗi: Vui lòng chỉ nhập số! Số không quá lớn.");
                 txtSo2.Focus();
                 txtSo2.SelectAll();
             }
-
         }
     }
 }
